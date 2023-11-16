95144b79d00bd0e67046e6eca6c646221d56a3f5
dbcook <caveduck17@gmail.com>
2023-11-15 22:42:31 -0800

    config: enable much debug and add documentation

--- Extended.config.h
+++ Extended.config.h
@@ -16,16 +16,27 @@
 // CONTROLLER ======================================================================================================================
 
 // DEBUG ---------------------------------------------------------------------------------------------------------------------------
-#define DEBUG                         OFF //    OFF, Use ON for background error messages only, use VERBOSE for all           Infreq
-// Enables additional debugging and/or status messages on the specified SERIAL_DEBUG port (be sure to disable when done!)
-// Note: The SERIAL_DEBUG port is often (almost always) attached to the same SERIAL_A port so it cannot be used for normal
+//   Debug log message configuration.
+//       DEBUG controls general diagnostics message level.
+//       DEBUG_xxx enables specific message sets.
+//       SERIAL_DEBUG sets the port used for debug output.
+//          On a Teensy4, Serial is a good choice since it goes back out the native USB interface and will not consume an actual serial port.
+//          Since there is a command processor listening to port Serial, you can type commands into OnStepX from the debug monitor.
+//          On ESP32, you need to choose a specific hardware port Serial1, Serial2, etc. that doesn't conflict with the one you use for the SWS or GPS.
+//       SERIAL_DEBUG_BAUD sets the data rate.  Ignored when using native USB port "Serial" on Teensy4.
+//       DEBUG_HARD_WAIT_FOR_MONITOR lets you force the system to wait for a connected serial monitor (e.g. from vscode) before startup.
+// Note: The SERIAL_DEBUG port is often attached to the same SERIAL_A port so it cannot be used for normal
 //       LX200 protocol communications.  If this is the case set both to the same (faster) baud rate specified below.
+#define DEBUG                         VERBOSE //    OFF, Use ON for background error messages only, use VERBOSE for all           Infreq
                                           //         error and status messages, use CONSOLE for VT100 debug console,
                                           //         or use PROFILER for VT100 task profiler.
+#define DEBUG_HARD_WAIT_FOR_MONITOR   OFF // Controls wait-for-debug-monitor connection behavior when DEBUG != OFF  
+                                          // OFF: do not wait, n: wait n seconds, ON: wait forever for serial monitor connect.
 #define DEBUG_SERVO                   OFF //    OFF, n. Where n=1 to 9 as the designated axis for logging servo activity.     Option
-#define DEBUG_ECHO_COMMANDS           OFF //    OFF, Use ON or ERRORS_ONLY to log commands to the debug serial port.          Option
-#define SERIAL_DEBUG               Serial // Serial, Use any available h/w serial port. Serial1 or Serial2, etc.              Option
+#define DEBUG_ECHO_COMMANDS           ON  //    OFF, Use ON or ERRORS_ONLY to log commands to the debug serial port.          Option
+#define SERIAL_DEBUG               Serial // Serial (For Teensy this is USB serial, baud ignored), Use any available h/w serial port. Serial1 or Serial2, etc.              Option
 #define SERIAL_DEBUG_BAUD          230400 // 230400, n. Where n=9600,19200,57600,115200,230400,460800 (common baud rates.)    Option
+                                          // No effect when SERIAL_DEBUG = Serial (for Teensy USB serial monitor channel)
 
 // NON-VOLATILE STORAGE ------------------------------------------------------------------------------------------------------------
 #define NV_WIPE                       OFF //         OFF, Causes the defaults to be written back into NV (FLASH,EEPROM,etc.)  Infreq
